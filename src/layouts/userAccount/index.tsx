4b46b21e1ab23b48747edbd9503b01034d281ad0
RajKoadiya22 <koladiyaraj22@gmail.com>
2025-04-11 11:00:28 +0530

    comment

--- src/layouts/userAccount/index.tsx
+++ src/layouts/userAccount/index.tsx
@@ -1,4 +1,4 @@
-import { AppLayout } from '../app';
+// import { AppLayout } from '../app';
 import {
   Col,
   ConfigProvider,
@@ -10,31 +10,35 @@ import {
   TabsProps,
   theme,
   Typography,
-} from 'antd';
-import { Card } from '../../components';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
-import { USER_PROFILE_ITEMS } from '../../constants';
-import { useStylesContext } from '../../context';
+} from "antd";
+import { Card } from "../../components";
+import {
+  // Outlet,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { USER_PROFILE_ITEMS } from "../../constants";
+import { useStylesContext } from "../../context";
 
 const { Link } = Typography;
 
-import './styles.css';
-import { useEffect, useState } from 'react';
+import "./styles.css";
+import { useEffect, useState } from "react";
 
-const DESCRIPTION_ITEMS: DescriptionsProps['items'] = [
+const DESCRIPTION_ITEMS: DescriptionsProps["items"] = [
   {
-    key: 'full-name',
-    label: 'Name',
+    key: "full-name",
+    label: "Name",
     children: <span>Kelvin Kiptum Kiprop</span>,
   },
   {
-    key: 'job-title',
-    label: 'Job title',
+    key: "job-title",
+    label: "Job title",
     children: <span>Software Engineer</span>,
   },
   {
-    key: 'email',
-    label: 'Email',
+    key: "email",
+    label: "Email",
     children: (
       <Link href="mailto:kelvin.kiprop96@gmail.com">
         kelvin.kiprop96@gmail.com
@@ -42,13 +46,13 @@ const DESCRIPTION_ITEMS: DescriptionsProps['items'] = [
     ),
   },
   {
-    key: 'telephone',
-    label: 'Phone',
+    key: "telephone",
+    label: "Phone",
     children: <Link href="tel:+254706094433">+254 706 094 4433</Link>,
   },
   {
-    key: 'github',
-    label: 'Github',
+    key: "github",
+    label: "Github",
     children: (
       <Link href="https://github.com/kelvink96" target="_blank">
         kelvink96
@@ -56,8 +60,8 @@ const DESCRIPTION_ITEMS: DescriptionsProps['items'] = [
     ),
   },
   {
-    key: 'twitter',
-    label: 'Twitter',
+    key: "twitter",
+    label: "Twitter",
     children: (
       <Link href="https://twitter.com/kelvink_96" target="_blank">
         @kelvink_96
@@ -66,7 +70,7 @@ const DESCRIPTION_ITEMS: DescriptionsProps['items'] = [
   },
 ];
 
-const TAB_ITEMS: TabsProps['items'] = USER_PROFILE_ITEMS.map((u) => ({
+const TAB_ITEMS: TabsProps["items"] = USER_PROFILE_ITEMS.map((u) => ({
   key: u.title,
   label: u.title,
 }));
@@ -87,7 +91,7 @@ export const UserAccountLayout = () => {
   useEffect(() => {
     console.log(location);
     const k =
-      TAB_ITEMS.find((d) => location.pathname.includes(d.key))?.key || '';
+      TAB_ITEMS.find((d) => location.pathname.includes(d.key))?.key || "";
 
     console.log(k);
     setActiveKey(k);
@@ -96,48 +100,48 @@ export const UserAccountLayout = () => {
   return (
     <>
       {/* <AppLayout> */}
-        <Card
-          className="user-profile-card-nav card"
-          actions={[
-            <ConfigProvider
-              theme={{
-                components: {
-                  Tabs: {
-                    colorBorderSecondary: 'none',
-                  },
+      <Card
+        className="user-profile-card-nav card"
+        actions={[
+          <ConfigProvider
+            theme={{
+              components: {
+                Tabs: {
+                  colorBorderSecondary: "none",
                 },
-              }}
-            >
-              <Tabs
-                defaultActiveKey={activeKey}
-                activeKey={activeKey}
-                items={TAB_ITEMS}
-                onChange={onChange}
-                style={{ textTransform: 'capitalize' }}
-              />
-            </ConfigProvider>,
-          ]}
-        >
-          <Row {...stylesContext?.rowProps}>
-            <Col xs={24} sm={8} lg={4}>
-              <Image
-                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                alt="user profile image"
-                height="100%"
-                width="100%"
-                style={{ borderRadius }}
-              />
-            </Col>
-            <Col xs={24} sm={16} lg={20}>
-              <Descriptions
-                title="User Info"
-                items={DESCRIPTION_ITEMS}
-                column={{ xs: 1, sm: 2, md: 2, lg: 3, xl: 3, xxl: 4 }}
-              />
-            </Col>
-          </Row>
-        </Card>
-        {/* <div style={{ marginTop: '1.5rem' }}>
+              },
+            }}
+          >
+            <Tabs
+              defaultActiveKey={activeKey}
+              activeKey={activeKey}
+              items={TAB_ITEMS}
+              onChange={onChange}
+              style={{ textTransform: "capitalize" }}
+            />
+          </ConfigProvider>,
+        ]}
+      >
+        <Row {...stylesContext?.rowProps}>
+          <Col xs={24} sm={8} lg={4}>
+            <Image
+              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="user profile image"
+              height="100%"
+              width="100%"
+              style={{ borderRadius }}
+            />
+          </Col>
+          <Col xs={24} sm={16} lg={20}>
+            <Descriptions
+              title="User Info"
+              items={DESCRIPTION_ITEMS}
+              column={{ xs: 1, sm: 2, md: 2, lg: 3, xl: 3, xxl: 4 }}
+            />
+          </Col>
+        </Row>
+      </Card>
+      {/* <div style={{ marginTop: '1.5rem' }}>
           <Outlet />
         </div> */}
       {/* </AppLayout> */}
